7a8d5a1509b1a2d7856a025ee085c321ca97d215
David Momenso <momenso@gmail.com>
2016-08-16 10:41:10 -0300

    disabled forced trace log

--- node-dht-sensor.cpp
+++ node-dht-sensor.cpp
@@ -171,10 +171,10 @@ long readDHT(int type, int pin, float &temperature, float &humidity)
     }
     else
     {
-//#ifdef VERBOSE
+#ifdef VERBOSE
         printf("Unexpected data: j=%d: %d != %d + %d + %d + %d\n",
                j, data[4], data[0], data[1], data[2], data[3]);
-//#endif
+#endif
         return -1;
     }
     
