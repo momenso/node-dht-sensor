da81ffea0e4e1d5307cd66572e8466433cd56862
David Momenso <momenso@gmail.com>
2016-10-24 11:03:45 -0200

    new test

---
+++ test/duty.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var sensor = require('../build/Release/node_dht_sensor');
+
+var usage = 'USAGE: node duty.js [sensorType] [gpioPin] <repeats>\n' +
+    '    sensorType:\n' +
+    '         11: For DHT11 sensor.\n' +
+    '         22: For DHT22 or AM2302 sensors.\n\n' +
+    '    gpipPin:\n' +
+    '         Pin number where the sensor is physically connected to.\n\n';
+
+if (process.argv.length < 4) {
+    console.warn(usage);
+    process.exit(1);
+}
+
+var sensorType = parseInt(process.argv[2], 10);
+var gpioPin = parseInt(process.argv[3], 10);
+var count = 0;
+var start = 0;
+var end = 0;
+
+var iid = setInterval(function() {
+  start = new Date().getTime();
+  sensor.read(sensorType, gpioPin, function(err, temperature, humidity) {
+  end = new Date().getTime();
+    if (err) {
+      console.warn('' + err);
+    } else {
+      var elapsed = end - start;
+      var result = 'temperature: ' + temperature.toFixed(1) + '°C, ' +
+                   'humidity: ' + humidity.toFixed(1) + '%, ' +
+                   'time: ' + elapsed + 'ms';
+      console.log(result);
+      fs.appendFile('log.csv',
+                  new Date().getTime() + ',' +
+                  temperature.toFixed(2) + ',' +
+                  humidity.toFixed(2) + ',' +
+                  elapsed + '\n',
+                  function(err) {});
+    }
+  });
+}, 2500);
