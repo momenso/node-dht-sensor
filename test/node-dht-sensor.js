7812d96012717d183f0d482e01c7d7e05fc36fcd
David Momenso <momenso@gmail.com>
2019-05-01 20:00:26 +0100

    parameterize sensor type and port for tests

--- test/node-dht-sensor.js
+++ test/node-dht-sensor.js
@@ -1,13 +1,16 @@
 var assert = require('chai').assert;
 var sensor = require('../build/Release/node_dht_sensor');
 
+const SENSOR_TYPE = parseInt(process.env.SENSOR_TYPE || 11, 10);
+const GPIO_PIN = parseInt(process.env.GPIO_PIN || 4, 10);
+
 describe('Initialize', () => {
     describe('Sensor type and GPIO pin', () => {
         it('should throw error if sensor type is not supported', () => {
             assert.throws(() => { sensor.initialize(0, 0); }, TypeError, 'Specified sensor type is not supported');
         });
         it('should initialize with valid sensor type and GPIO pin', () => {
-            sensor.initialize(11, 4);
+            sensor.initialize(SENSOR_TYPE, GPIO_PIN);
         });
     });
     describe('With invalid arguments', () => {
@@ -18,10 +21,10 @@ describe('Initialize', () => {
             assert.throws(() => { sensor.initialize('11', 0); }, TypeError, 'Invalid arguments');
         })
         it('should fail when GPIO pin is not numeric', () => {
-            assert.throws(() => { sensor.initialize(11, '4'); }, TypeError, 'Invalid arguments');
+            assert.throws(() => { sensor.initialize(SENSOR_TYPE, '4'); }, TypeError, 'Invalid arguments');
         })
         it('should fail when maxRetries is not numeric', () => {
-            assert.throws(() => { sensor.initialize(11, 4, 'abc'); }, TypeError, 'Invalid maxRetries parameter');
+            assert.throws(() => { sensor.initialize(SENSOR_TYPE, GPIO_PIN, 'abc'); }, TypeError, 'Invalid maxRetries parameter');
         })
     });
 });
@@ -38,31 +41,31 @@ describe('Set max retries', () => {
 describe('Read sensor', () => {
     describe('Synchronously', () => {
         it ('should return a readout when no parameter is provided', () => {
-            sensor.initialize(11, 4);
+            sensor.initialize(SENSOR_TYPE, GPIO_PIN);
             var readout = sensor.read();
 
             assert.isObject(readout);
             assert.hasAllKeys(readout, ['temperature', 'humidity', 'isValid', 'errors']);
         });
         it('should return a readout when sensor type and GPIO pin are provided', () => {
-            var readout = sensor.read(11, 4);
+            var readout = sensor.read(SENSOR_TYPE, GPIO_PIN);
             assert.isObject(readout);
             assert.hasAllKeys(readout, ['temperature', 'humidity', 'isValid', 'errors']);
         });
         it('should fail when invalid sensor type is specified', () => {
-            assert.throws(() => { sensor.read(3, 4); }, TypeError, 'specified sensor type is invalid');
+            assert.throws(() => { sensor.read(3, GPIO_PIN); }, TypeError, 'specified sensor type is invalid');
         });
     });
     describe('Asynchronously', () => {
         it('should obtain temperature and humidity', () => {
-            sensor.read(11, 4, (err, temperature, humidity) => {
+            sensor.read(SENSOR_TYPE, GPIO_PIN, (err, temperature, humidity) => {
                 assert.isNull(err);
                 assert.isNumber(temperature);
                 assert.isNumber(humidity);
             });
         });
         it('should fail when invalid sensor type is specified', () => {            
-            sensor.read(3, 4, (err) => {
+            sensor.read(3, GPIO_PIN, (err) => {
                 assert.isNotNull(err);
                 assert.throws(() => {
                     assert.ifError(err, 'sensor type is invalid')
