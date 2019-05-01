020e69567f5f193a43155c32d02e29046546ab46
David Momenso <momenso@gmail.com>
2019-05-01 23:53:19 +0100

    adds fake test example

---
+++ examples/fake-test.js
@@ -0,0 +1,18 @@
+var sensor = require('../build/Release/node_dht_sensor');
+
+sensor.initialize({
+	test : {
+		fake: {
+			temperature: 21,
+			humidity: 60
+		}
+	}
+});
+
+sensor.read(22, 4, function(err, temperature, humidity) {
+    if (!err) {
+        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+            'humidity: ' + humidity.toFixed(1) + '%'
+        );
+    }
+});
